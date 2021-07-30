26b4cb23a200ecc0f8128d20af8a0eb8b4f7ee71
Audrey <audrey.gillies@gmail.com>
2021-07-29 22:58:32 -0600

    pseudo-code created in an md file.  JavaScript file working through the part of storing the user input for character length.  Need to add more pieces to the method generatePassword to finish the project.

---
+++ pseudo-code.md
@@ -0,0 +1,31 @@
+## Steps a user takes will have numbers.
+
+## What we want to happen visually for the user will be below the step, indented.
+
+### 1) Clicks the 'generate password' button.
+
+    Message appears to user stating the required criteria/decisions to be made. The message also lets the user know that, once the criteria has been met, a password will generate and will be displayed to the screen. If they are not in a secure location, they should re-visit this site when they can have the new password displayed on their screen.
+
+### 2) Clicks to confirm they want to continue.
+
+    Message appears asking for the length of the password, stating the user must input a value between 8 and 128, inclusive.
+
+### 3) Types a password length using a number greater than or equal to 8, but less than or equal to 128.
+
+    Message appears validating the number was acceptable, and re-states the choice for the user.
+
+### 4) Clicks to confirm the computer correctly stated their number.
+
+    Message appears asking for the character types the user is wanting to include in the password. The options shown include lowercase, uppercase, numeric, and special characters. The user must select at least one, but could choose up to all four.
+
+### 5) Types which character-types to include.
+
+    Message appears validating the choice made was acceptable, and re-states the choice for the user.
+
+### 6) Clicks to confirm the computer correctly stated their choice(s).
+
+    Message appears stating the password has been generated and asks the user to click 'ok' to confirm the appearance of the password. If the user does not want the password to be displayed, all 'data' will be lost and they will not receive a new password.
+
+### 7) Clicks to confirm they want the password displayed.
+
+    The newly generated password is displayed in the box, meeting the users criteria.
--- script.js
+++ script.js
@@ -1,14 +1,56 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateButton = document.querySelector("#generate");
+var inputCharacterType = [
+  "uppercase",
+  "lowercase",
+  "numbers",
+  "special characters",
+];
+inputLength = 0;
+rightLength = false;
+
+var createPassword = {
+  characterLength: inputLength,
+  characterType: inputCharacterType,
+  generatePassword: function () {
+    var readyToCreate = confirm(
+      "You will be generating a new password. You will be asked to type the number of characters and they type of characters you want for your new password. When we are finished, your password will be displayed onto the screen.  If you are in a public location and do not wish for your password to appear on the screen, please return to this page when you are comfortable with the password appearing on the screen. To continue, click 'ok.' If you wish to try later, click 'cancel.'"
+    );
+
+    if (readyToCreate === true) {
+      var inputLength = prompt(
+        "How many characters do you want your new password to have?  Please type a whole number greater than 7 and less than 129.",
+        "Number"
+      );
+    } else {
+      window.alert("See you later!");
+      return;
+    }
+    while (rightLength === false) {
+      if (inputLength >= 8 && inputLength <= 128) {
+        window.alert(
+          "I see that you want your password to have " +
+            inputLength +
+            " characters in length.  Click 'ok' to continue."
+        );
+        rightLength = true;
+      } else {
+        inputLength = prompt(
+          "It appear you entered an invalid response.  How many characters do you want your new password to have?  Please type a whole number greater than 7 and less than 129.",
+          "Number"
+        );
+      }
+    }
+  },
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = createPassword.generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateButton.addEventListener("click", writePassword);
