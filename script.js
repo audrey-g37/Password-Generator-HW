9b27a585a4beddacf85f6eb7575a7ba9b5eac3db
Audrey <audrey.gillies@gmail.com>
2021-07-31 14:26:57 -0600

    took out prompts for user to select characters and put in confirm messages instead. inputLength is storing as a number; _______Inclusion are storing as boolean values.  Need to actually get password to generate based on user input.  Pseudo-code md file updated.

--- script.js
+++ script.js
@@ -11,24 +11,6 @@ var rightLength = false;
 var howManyCharactersMessage =
   "How many characters do you want your newly generated password to have?  Please type a whole number greater than 7 and less than 129.";
 
-var numberInclusionMessage =
-  "Would you like to include numbers in your newly generated password? Please type 'yes' or 'no' in the box below (with no capital letters and without the quotation marks).  Remember, you must type 'yes' to at least one of the character type messages.";
-var lowerCaseInclusionMessage =
-  "Would you like to include lower-case letters in your newly generated password? Please type 'yes' or 'no' in the box below (without the quotation marks).  Remember, you must type 'yes' to at least one of the character type messages.";
-var upperCaseInclusionMessage =
-  "Would you like to include upper-case letters in your newly generated password? Please type 'yes' or 'no' in the box below (without the quotation marks).  Remember, you must type 'yes' to at least one of the character-type messages.";
-var specialCharacterInclusionMessage =
-  "Would you like to include special characters in your newly generated password? Please type 'yes' or 'no' in the box below (without the quotation marks).  Remember, you must type 'yes' to at least one of the character-type messages.";
-
-var CharacterTypesMessage =
-  "numbers, lowercase letters, uppercase letters, and special characters.";
-
-var inputCharacterTypes = [
-  "numbers",
-  "lowercase letters",
-  "uppercase letters",
-  "special characters",
-];
 var possibleNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var possibleLowerCaseLetters = [
   "a",
@@ -98,46 +80,22 @@ var possibleSpecialCharacters = [
   " ",
 ];
 
-function charactersToInclude(
-  characterType,
-  characterMessage,
-  validResponse,
-  type
-) {
-  while (validResponse === false) {
-    characterType = prompt(characterMessage);
-    if (characterType == "yes") {
-      characterType = true;
-      alert(
-        "I see that you DO want your password to include " +
-          inputCharacterTypes[type] +
-          ". Click 'ok' to continue."
-      );
-      validResponse = true;
-    } else if (characterType == "no") {
-      characterType = false;
-      alert(
-        "I see that you do NOT want your password to include " +
-          inputCharacterTypes[type] +
-          ". Click 'ok' to continue."
-      );
-      validResponse = true;
-    } else {
-      alert(invalidMessage);
-    }
-  }
-}
+var inputCharacterTypes = [
+  "numbers",
+  "lower-case letters",
+  "upper-case letters",
+  "special characters",
+];
+
+var numberInclusion = false;
+var lowerCaseInclusion = false;
+var upperCaseInclusion = false;
+var specialCharacterInclusion = false;
 
 function generatePassword() {
-  var validNumberInclusionResponse = false;
-  var validLowerCaseInclusionResponse = false;
-  var validUpperCaseInclusionResponse = false;
-  var validSpecialCharacterInclusionResponse = false;
+  var CharacterTypesMessage =
+    "numbers, lowercase letters, uppercase letters, and special characters.";
 
-  var numberInclusion = false;
-  var lowerCaseInclusion = false;
-  var upperCaseInclusion = false;
-  var specialCharacterInclusion = false;
   var readyToCreate = confirm(
     "I will be generating a new password for you. You will be asked to type a number of characters, and to confirm what type of characters you want to be included for your new password. When we are finished, your password will be displayed onto the screen.  If you are in a public location and do not wish for your newly generated password to appear on the screen, please return to this page when you are comfortable with the password appearing on the screen. To continue, click 'ok.' If you wish to try later, click 'cancel.'"
   );
@@ -158,9 +116,6 @@ function generatePassword() {
           " characters in length.  Click 'ok' to continue."
       );
       rightLength = true;
-    } else if (inputLength == null) {
-      alert(cancelButtonMessage);
-      location.reload();
     } else {
       inputLength = prompt(invalidMessage + howManyCharactersMessage, "Number");
     }
@@ -168,53 +123,64 @@ function generatePassword() {
   alert(
     "Please read! After you click 'ok,' you will be asked which character types you want me to include in your newly generated password. There are four possible character types: " +
       CharacterTypesMessage +
-      " You will receive a message about each character type.  If you want to include that character type, you will type 'yes' into the box.  If you do NOT want to include that character type, you will type 'no' into the box.  PLEASE NOTE: You must include at least one character type, so you must type 'yes' at least once.  You may include more than one character type by typing 'yes' in more than one of the message boxes."
-  );
-  charactersToInclude(
-    numberInclusion,
-    numberInclusionMessage,
-    validNumberInclusionResponse,
-    0
-  );
-  charactersToInclude(
-    lowerCaseInclusion,
-    lowerCaseInclusionMessage,
-    validLowerCaseInclusionResponse,
-    1
-  );
-  charactersToInclude(
-    upperCaseInclusion,
-    upperCaseInclusionMessage,
-    validUpperCaseInclusionResponse,
-    2
-  );
-  charactersToInclude(
-    specialCharacterInclusion,
-    specialCharacterInclusionMessage,
-    validSpecialCharacterInclusionResponse,
-    3
+      ".\n You will receive a message about each character type (four alerts total).  If you DO want to include that character type, you will click 'ok.'  If you DO NOT want to include that character type, you will click cancel.  \n **PLEASE NOTE: You must include at least one character type, so you must click 'ok' at least once.  You may include more than one character type by clicking 'ok' on more than one of the message alerts."
   );
 
-  console.log(inputLength);
-  console.log(numberInclusion);
-  console.log(lowerCaseInclusion);
-  console.log(upperCaseInclusion);
-  console.log(specialCharacterInclusion);
-  console.log(validNumberInclusionResponse);
+  function inclusionMessage(characterType, typeIndexNumber) {
+    var characterType = confirm(
+      "Would you like to include " +
+        inputCharacterTypes[typeIndexNumber] +
+        " in your newly generated password? If you DO want " +
+        inputCharacterTypes[typeIndexNumber] +
+        " please click 'ok.'  If you DO NOT want " +
+        inputCharacterTypes[typeIndexNumber] +
+        " , please click 'cancel.'  Remember, you must include at least one character type in your newly generated password."
+    );
+    if (characterType === true) {
+      alert(
+        "I see that you DO want your password to include " +
+          inputCharacterTypes[typeIndexNumber] +
+          ". Click 'ok' to continue."
+      );
+      return characterType;
+    } else {
+      alert(
+        "I see that you DO NOT want your password to include " +
+          inputCharacterTypes[typeIndexNumber] +
+          ". Click 'ok' to continue."
+      );
+      return characterType;
+    }
+  }
+  function characterInclusion() {
+    numberInclusion = inclusionMessage(numberInclusion, 0);
+    lowerCaseInclusion = inclusionMessage(lowerCaseInclusion, 1);
+    upperCaseInclusion = inclusionMessage(upperCaseInclusion, 2);
+    specialCharacterInclusion = inclusionMessage(specialCharacterInclusion, 3);
+  }
 
-  var createPassword = {
-    choiceOfCharacterLength: inputLength,
-    choiceOfNumberCharacters: numberInclusion,
-    choiceOfLowerCaseInclusion: lowerCaseInclusion,
-    choiceOfUpperCaseInclusion: upperCaseInclusion,
-    choiceOfSpecialCharacterInclusion: specialCharacterInclusion,
-  };
+  characterInclusion();
+
+  while (
+    numberInclusion === false &&
+    lowerCaseInclusion === false &&
+    upperCaseInclusion === false &&
+    specialCharacterInclusion === false
+  ) {
+    alert(invalidMessage);
+    characterInclusion();
+  }
+
+alert("Your new password has been generated.  Click 'ok' to have your password appear on the screen.  Make sure to record your password and keep it private.")
+
+function () {
+
+
+
+};
+
+passwordOutput = newPassword;
+
 }
 
-function writePassword() {
-  var password = generatePassword();
-
-  passwordOutput = password;
-}
-
-generateButton.addEventListener("click", writePassword);
+generateButton.addEventListener("click", generatePassword);
