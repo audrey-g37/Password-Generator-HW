4403b1bf4153408d65d3d38834bfa7f607c39b19
Audrey <audrey.gillies@gmail.com>
2021-08-01 00:03:05 -0600

    characters are generating randomly.  Pseudo-code md file updated.  shuffle function still needs to be worked on to shuffle the random characters (it's created, but not functioning properly, so currently commented out

--- pseudo-code.md
+++ pseudo-code.md
@@ -14,18 +14,30 @@
 
     Message appears validating the number was acceptable, and re-states the choice for the user.
 
-### 4) Clicks to confirm the computer correctly stated their number.
+### 4) Clicks 'ok' to validate the computer correctly received their number.
 
     Message appears asking for the character types the user is wanting to include in the password. The options shown include lowercase, uppercase, numeric, and special characters. The user must select at least one, but could choose up to all four.
 
-### 5) Types which character-types to include.
+### 5) Clicks 'ok' to confirm understanding.
 
-    Message appears validating the choice made was acceptable, and re-states the choice for the user.
+    Message appears asking user to choose if they want to include numbers in the password.
 
-### 6) Clicks to confirm the computer correctly stated their choice(s).
+### 6) Clicks 'ok' to include numbers or clicks cancel to exclude numbers.
+
+    Message appears asking user to choose if they want to include lower-case letters in the password.
+
+### 7) Clicks 'ok' to include lower-case letters or clicks cancel to exclude lower-case letters.
+
+    Message appears asking user to choose if they want to include upper-case letters in the password.
+
+### 8) Clicks 'ok' to include upper-case letters or clicks cancel to exclude upper-case letters.
+
+    Message appears asking user to choose if they want to include special characters in the password.
+
+### 9) Clicks 'ok' to include special characters or clicks cancel to exclude special characters.
 
     Message appears stating the password has been generated and asks the user to click 'ok' to confirm the appearance of the password. If the user does not want the password to be displayed, all 'data' will be lost and they will not receive a new password.
 
-### 7) Clicks to confirm they want the password displayed.
+### 11) Clicks 'ok' to confirm they want the password displayed.
 
     The newly generated password is displayed in the box, meeting the users criteria.
--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 // Assignment Code
 var generateButton = document.querySelector("#generate");
 var passwordOutput = document.querySelector("#password");
+generateButton.addEventListener("click", generatePassword);
 
 var invalidMessage = "It appears you have entered an invalid response. ";
 var cancelButtonMessage = "See you later!";
 
-var inputLength = 0;
-var rightLength = false;
-
 var howManyCharactersMessage =
   "How many characters do you want your newly generated password to have?  Please type a whole number greater than 7 and less than 129.";
 
-var possibleNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var possibleNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var possibleLowerCaseLetters = [
   "a",
   "b",
@@ -87,26 +85,41 @@ var inputCharacterTypes = [
   "special characters",
 ];
 
+var inputLength = 0;
+var rightLength = false;
+
 var numberInclusion = false;
 var lowerCaseInclusion = false;
 var upperCaseInclusion = false;
 var specialCharacterInclusion = false;
 
-function generatePassword() {
-  var CharacterTypesMessage =
-    "numbers, lowercase letters, uppercase letters, and special characters.";
+var chooseNumbers = [""];
+var chooseLowerCase = [""];
+var chooseUpperCase = [""];
+var chooseSpecialCharacters = [""];
 
+var finalNumbers = [""];
+var finalLowerCase = [""];
+var finalUpperCase = [""];
+var finalSpecialCharacters = [""];
+
+function generatePassword() {
   var readyToCreate = confirm(
     "I will be generating a new password for you. You will be asked to type a number of characters, and to confirm what type of characters you want to be included for your new password. When we are finished, your password will be displayed onto the screen.  If you are in a public location and do not wish for your newly generated password to appear on the screen, please return to this page when you are comfortable with the password appearing on the screen. To continue, click 'ok.' If you wish to try later, click 'cancel.'"
   );
 
-  if (readyToCreate === true) {
-    inputLength = prompt(howManyCharactersMessage, "Number");
-  } else {
+  if (readyToCreate === false) {
     alert(cancelButtonMessage);
-    passwordOutput = "";
+    passwordOutput.value = "";
     return;
   }
+
+  function passwordLength() {
+    inputLength = prompt(howManyCharactersMessage, "Number");
+    return inputLength;
+  }
+
+  passwordLength();
 
   while (rightLength === false) {
     if (inputLength >= 8 && inputLength <= 128 && inputLength % 1 === 0) {
@@ -117,13 +130,13 @@ function generatePassword() {
       );
       rightLength = true;
     } else {
-      inputLength = prompt(invalidMessage + howManyCharactersMessage, "Number");
+      alert(invalidMessage);
+      passwordLength();
     }
   }
+
   alert(
-    "Please read! After you click 'ok,' you will be asked which character types you want me to include in your newly generated password. There are four possible character types: " +
-      CharacterTypesMessage +
-      ".\n You will receive a message about each character type (four alerts total).  If you DO want to include that character type, you will click 'ok.'  If you DO NOT want to include that character type, you will click cancel.  \n **PLEASE NOTE: You must include at least one character type, so you must click 'ok' at least once.  You may include more than one character type by clicking 'ok' on more than one of the message alerts."
+    "Please read! After you click 'ok,' you will be asked which character types you want me to include in your newly generated password. There are four possible character types:\n1) numbers\n2) lowercase letters\n3) uppercase letters\n4) special characters\nYou will receive a message about each character type (four alerts total).  If you DO want to include that character type, click 'ok.'  If you DO NOT want to include that character type, click cancel.\n **PLEASE NOTE: You must include at least one character type, so you must click 'ok' at least once.  You may include more than one character type by clicking 'ok' on more than one of the message alerts."
   );
 
   function inclusionMessage(characterType, typeIndexNumber) {
@@ -167,20 +180,125 @@ function generatePassword() {
     upperCaseInclusion === false &&
     specialCharacterInclusion === false
   ) {
-    alert(invalidMessage);
+    alert(
+      invalidMessage +
+        "You must have at least one character type in your newly generated password. The four messages will appear again. Please click 'ok' at least once."
+    );
     characterInclusion();
   }
 
-alert("Your new password has been generated.  Click 'ok' to have your password appear on the screen.  Make sure to record your password and keep it private.")
+  function includedCharacters(inclusionType, chosenType, possibleType) {
+    if (inclusionType === true) {
+      chosenType = possibleType;
+      return chosenType;
+    } else {
+      chosenType = "";
+      return chosenType;
+    }
+  }
 
-function () {
+  function chosenCharacters() {
+    chooseNumbers = includedCharacters(
+      numberInclusion,
+      chooseNumbers,
+      possibleNumbers
+    );
+    chooseLowerCase = includedCharacters(
+      lowerCaseInclusion,
+      chooseLowerCase,
+      possibleLowerCaseLetters
+    );
+    chooseUpperCase = includedCharacters(
+      upperCaseInclusion,
+      chooseUpperCase,
+      possibleUpperCaseLetters
+    );
+    chooseSpecialCharacters = includedCharacters(
+      specialCharacterInclusion,
+      chooseSpecialCharacters,
+      possibleSpecialCharacters
+    );
+  }
 
+  chosenCharacters();
 
+  var passwordLength = Number(inputLength);
 
-};
+  function finalCharacterSelection(characterSelection, chosenType) {
+    var characterSelection =
+      chosenType[Math.floor(Math.random() * chosenType.length)];
+    return characterSelection;
+  }
 
-passwordOutput = newPassword;
+  function finalChosenCharacters() {
+    finalNumbers = finalCharacterSelection(finalNumbers, chooseNumbers);
+    finalLowerCase = finalCharacterSelection(finalLowerCase, chooseLowerCase);
+    finalUpperCase = finalCharacterSelection(finalUpperCase, chooseUpperCase);
+    finalSpecialCharacters = finalCharacterSelection(
+      finalSpecialCharacters,
+      chooseSpecialCharacters
+    );
+  }
 
+  var newlyGeneratedPassword = "";
+
+  console.log(chooseNumbers);
+  console.log(chooseLowerCase);
+  console.log(chooseUpperCase);
+  console.log(chooseSpecialCharacters);
+
+  while (newlyGeneratedPassword.length < passwordLength) {
+    finalChosenCharacters();
+
+    if (chooseNumbers == "") {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat("");
+    } else if (newlyGeneratedPassword.length !== passwordLength) {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat(finalNumbers);
+      console.log(newlyGeneratedPassword);
+    }
+    if (chooseLowerCase == "") {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat("");
+    } else if (newlyGeneratedPassword.length !== passwordLength) {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat(finalLowerCase);
+      console.log(newlyGeneratedPassword);
+    }
+    if (chooseUpperCase == "") {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat("");
+    } else if (newlyGeneratedPassword.length !== passwordLength) {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat(finalUpperCase);
+      console.log(newlyGeneratedPassword);
+    }
+    if (chooseSpecialCharacters == "") {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat("");
+    } else if (newlyGeneratedPassword.length !== passwordLength) {
+      newlyGeneratedPassword = newlyGeneratedPassword.concat(
+        finalSpecialCharacters
+      );
+      console.log(newlyGeneratedPassword);
+    }
+  }
+
+  alert(
+    "Your new password has been generated.  Click 'ok' to have your password appear on the screen.  Make sure to record your password and keep it private."
+  );
+
+  //   var finalGeneratedPassword = "";
+
+  //   function shuffle() {
+  //     finalGeneratedPassword = newlyGeneratedPassword.split("");
+  //     for (var i = passwordLength - 1; i > 0; i--) {
+  //       var selectingCharacters = Math.floor(Math.random() * (i + 1));
+  //       console.log(selectingCharacters);
+  //       var whatDoIDo = finalGeneratedPassword[i];
+  //       console.log(whatDoIDo);
+  //       finalGeneratedPassword[i] = finalGeneratedPassword[selectingCharacters];
+  //       finalGeneratedPassword[selectingCharacters] = whatDoIDo;
+  //       finalGeneratedPassword.join("");
+  //     }
+  //     return finalGeneratedPassword;
+  //   }
+
+  //   shuffle();
+
+  //   passwordOutput.value = finalGeneratedPassword;
 }
-
-generateButton.addEventListener("click", generatePassword);
