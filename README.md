87b0605ddd3a8ce43743fe3181641058a3df7897
Audrey <audrey.gillies@gmail.com>
2021-08-01 23:59:54 -0600

    found a bug where if user said no numbers, they didn't get other random characters.  That has been fixed.  Comments added to javascript file.  README completed.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Password-Generator-HW
+
+## Description
+
+A user can answer some prompts, and the computer will generate a password for them. The user must meet a few criteria when making their choices. The password cannot be shorter than 8 characters and cannot be longer than 128 characters. The user will be asked to select what character types to include; given the options of numbers, lower-case letters, upper-case letters, and special characters. The user must choose at least one character type. If at any point the user doesn't 'meet the criteria' with their input, the program will alert them. It will then continue to ask the user to make a decision that meets the criteria. At the end, the password will appear on the screen.
+
+## Assets
+
+The following image is a screenshot of the html file loaded in a browser. It includes a password that was generated with the process.
+
+![Screenshot of the Website](./Assets/Random-Password.png)
+
+## Link to Live Url
+
+Click below to open the actual link.
+
+[The Password Generator](https://audrey-g37.github.io/Password-Generator-HW/)
--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-// Assignment Code
+// Variables declared accessing elements in the html File.
+
 var generateButton = document.querySelector("#generate");
 var passwordOutput = document.querySelector("#password");
+
+// The button will work and will run the 'generatePassword' function when clicked.
+
 generateButton.addEventListener("click", generatePassword);
+
+// Messages that are commonly used are declared as variables for easy reference.
 
 var invalidMessage = "It appears you have entered an invalid response. ";
 var cancelButtonMessage = "See you later!";
-
 var howManyCharactersMessage =
   "How many characters do you want your newly generated password to have?  Please type a whole number greater than 7 and less than 129.";
 
+//Arrays created to pull from when randomly generating the password.
 var possibleNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var possibleLowerCaseLetters = [
   "a",
@@ -78,6 +84,7 @@ var possibleSpecialCharacters = [
   " ",
 ];
 
+//Array declared to help simplify the function.
 var inputCharacterTypes = [
   "numbers",
   "lower-case letters",
@@ -85,6 +92,7 @@ var inputCharacterTypes = [
   "special characters",
 ];
 
+//Variables declared as a 'starting' value... value changes in the function.
 var inputLength = 0;
 var rightLength = false;
 
@@ -103,17 +111,21 @@ var finalLowerCase = [""];
 var finalUpperCase = [""];
 var finalSpecialCharacters = [""];
 
+//Function that generates a new password.
 function generatePassword() {
+  //Message 1 asking user to confirm they want to generate a password.
   var readyToCreate = confirm(
     "I will be generating a new password for you. You will be asked to type a number of characters, and to confirm what type of characters you want to be included for your new password. When we are finished, your password will be displayed onto the screen.  If you are in a public location and do not wish for your newly generated password to appear on the screen, please return to this page when you are comfortable with the password appearing on the screen. To continue, click 'ok.' If you wish to try later, click 'cancel.'"
   );
 
+  //If user says 'no/cancel' to the message above, they are 'exited out.'
   if (readyToCreate === false) {
     alert(cancelButtonMessage);
     passwordOutput.value = "";
     return;
   }
 
+  //Function declared to get user input on password length.  Called once, then repeatedly called if user gives an invalid answer/choice.
   function passwordLength() {
     inputLength = prompt(howManyCharactersMessage, "Number");
     return inputLength;
@@ -135,6 +147,7 @@ function generatePassword() {
     }
   }
 
+  //Instruction message for the user followed by messages asking about character types to include.  If the user doesn't select any characters, the process of character-type selection repeats.
   alert(
     "Please read! After you click 'ok,' you will be asked which character types you want me to include in your newly generated password. There are four possible character types:\n1) numbers\n2) lowercase letters\n3) uppercase letters\n4) special characters\nYou will receive a message about each character type (four alerts total).  If you DO want to include that character type, click 'ok.'  If you DO NOT want to include that character type, click cancel.\n **PLEASE NOTE: You must include at least one character type, so you must click 'ok' at least once.  You may include more than one character type by clicking 'ok' on more than one of the message alerts."
   );
@@ -187,6 +200,7 @@ function generatePassword() {
     characterInclusion();
   }
 
+  // Depending on how the user answered the character inclusion messages, the choices they made are stored and variables will now contain the array of characters (or an empty array if the user did not want to include that type).
   function includedCharacters(inclusionType, chosenType, possibleType) {
     if (inclusionType === true) {
       chosenType = possibleType;
@@ -222,14 +236,17 @@ function generatePassword() {
 
   chosenCharacters();
 
+  //The user's input was a string, so the code below changes its data type to a number.
   var passwordLength = Number(inputLength);
 
+  //The function below allows a random character to be pulled from each array.
   function finalCharacterSelection(characterSelection, chosenType) {
     var characterSelection =
       chosenType[Math.floor(Math.random() * chosenType.length)];
     return characterSelection;
   }
 
+  //The function below accesses the above function so that we can repeat pulling one character/string from an array.
   function finalChosenCharacters() {
     finalNumbers = finalCharacterSelection(finalNumbers, chooseNumbers);
     finalLowerCase = finalCharacterSelection(finalLowerCase, chooseLowerCase);
@@ -242,11 +259,7 @@ function generatePassword() {
 
   var newlyGeneratedPassword = "";
 
-  console.log(chooseNumbers);
-  console.log(chooseLowerCase);
-  console.log(chooseUpperCase);
-  console.log(chooseSpecialCharacters);
-
+  //This while loop adds characters to the password until it matches the length that the user chose.
   while (newlyGeneratedPassword.length < passwordLength) {
     finalChosenCharacters();
 
@@ -254,19 +267,16 @@ function generatePassword() {
       newlyGeneratedPassword = newlyGeneratedPassword.concat("");
     } else if (newlyGeneratedPassword.length !== passwordLength) {
       newlyGeneratedPassword = newlyGeneratedPassword.concat(finalNumbers);
-      console.log(newlyGeneratedPassword);
     }
     if (chooseLowerCase == "") {
       newlyGeneratedPassword = newlyGeneratedPassword.concat("");
     } else if (newlyGeneratedPassword.length !== passwordLength) {
       newlyGeneratedPassword = newlyGeneratedPassword.concat(finalLowerCase);
-      console.log(newlyGeneratedPassword);
     }
     if (chooseUpperCase == "") {
       newlyGeneratedPassword = newlyGeneratedPassword.concat("");
     } else if (newlyGeneratedPassword.length !== passwordLength) {
       newlyGeneratedPassword = newlyGeneratedPassword.concat(finalUpperCase);
-      console.log(newlyGeneratedPassword);
     }
     if (chooseSpecialCharacters == "") {
       newlyGeneratedPassword = newlyGeneratedPassword.concat("");
@@ -274,7 +284,6 @@ function generatePassword() {
       newlyGeneratedPassword = newlyGeneratedPassword.concat(
         finalSpecialCharacters
       );
-      console.log(newlyGeneratedPassword);
     }
   }
 
@@ -282,23 +291,6 @@ function generatePassword() {
     "Your new password has been generated.  Click 'ok' to have your password appear on the screen.  Make sure to record your password and keep it private."
   );
 
-  //   var finalGeneratedPassword = "";
-
-  //   function shuffle() {
-  //     finalGeneratedPassword = newlyGeneratedPassword.split("");
-  //     for (var i = passwordLength - 1; i > 0; i--) {
-  //       var selectingCharacters = Math.floor(Math.random() * (i + 1));
-  //       console.log(selectingCharacters);
-  //       var whatDoIDo = finalGeneratedPassword[i];
-  //       console.log(whatDoIDo);
-  //       finalGeneratedPassword[i] = finalGeneratedPassword[selectingCharacters];
-  //       finalGeneratedPassword[selectingCharacters] = whatDoIDo;
-  //       finalGeneratedPassword.join("");
-  //     }
-  //     return finalGeneratedPassword;
-  //   }
-
-  //   shuffle();
-
-  //   passwordOutput.value = finalGeneratedPassword;
+  //The new password will be displayed on the screen.
+  passwordOutput.value = newlyGeneratedPassword;
 }
